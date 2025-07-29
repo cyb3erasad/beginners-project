a1b32bcd0d1afc74fdecf7ae66ecefd7c0c3def8
Cyb3r Asad <cyb3rasad@gmail.com>
2025-07-29 22:07:50 +0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-
-app = QApplication([])
-window = QWidget()
-window.setWindowTitle("Hello PyQt5")
-window.resize(300, 200)
-window.show()
-app.exec_()
