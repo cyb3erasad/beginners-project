0a590161757533b02e1d44e956f3ee6fa21fb0c4
Cyb3r Asad <cyb3rasad@gmail.com>
2025-09-01 19:45:21 +0500

    Add files via upload
    When you use this program, use write key function only once a time for generate a key

---
+++ password_manager.py
@@ -0,0 +1,54 @@
+from cryptography.fernet import Fernet
+
+# def write_key():
+#     key = Fernet.generate_key()
+#     with open("Key.Key", 'wb') as key_file:
+#         key_file.write(key)
+
+
+def load_key():
+    file = open("Key.Key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+
+
+
+def view():
+    with open("passwords.txt", 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
+
+def add():
+    name = input("Enter Account Name: ")
+    pwd = input("Enter Password: ")
+
+    with open("passwords.txt", 'a') as f:
+       f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+       
+
+
+
+while True:
+    mode = input("Would you like to add a new password or view existince ones? (view, add) press q to exit: ").lower()
+
+    if mode == 'q':
+        break
+    if mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print("Invalid mode!")
+        continue        
