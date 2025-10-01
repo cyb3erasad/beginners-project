0cd287c7b57612da34104c4c2994d1042ba310b3
Cyb3r Asad <cyb3rasad@gmail.com>
2025-10-01 22:33:40 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import json
+
+def load_data():
+        try:
+          with open("youtube.txt", 'r') as file:
+             return json.load(file)
+
+        except FileNotFoundError:
+            return []
+        
+def save_data(videos):
+   with open('youtube.txt', 'w') as file:
+      json.dump(videos, file) 
+
+def list_all_videos(videos):
+   for index, video in enumerate(videos, start=1):
+      print(f"\n{index}. {video['name']} | Duration: {video['time']}")
+
+def add_yt_video(videos):
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name': name, 'time': time})
+    save_data(videos)
+
+def update_yt_video(videos):
+    pass
+
+def delete_yt_video(videos):
+    pass
+def main():
+    videos = load_data()
+    while True:
+        print("\n=== Youtube Manager | Choose an option ===\n")
+        print("1. List all youtube videos")
+        print("2. Add a youtube video")
+        print("3. Update youtube video details")
+        print("4. Delete youtube video details")
+        print("5. Exit the app")
+        choose = input("Enter your choice: ")
+
+        match choose:
+            case '1':
+              list_all_videos(videos)
+            case '2':
+              add_yt_video(videos)
+            case '3':
+              update_yt_video(videos)
+            case '4':
+              delete_yt_video(videos)
+            case '5':
+              break
+            case _:
+              print("Invalid choice please try again!")
+
+if __name__ == "__main__":
+    main()                             
